19a2f8bf5627f12ee27b13b13ba92dc67de5c3ad
tomaspalica <tomasss.palica@gmail.com>
2023-08-19 19:00:35 +0200

    step 2 in proces

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -4,25 +4,36 @@ class Feedback extends Component{
     state = {
         good: 0,
         neutral: 0,
-        bad: 0
+        bad: 0,
+        total: 0
       }
 
       hendleClick = (evt) =>{
  const {name,value} = evt.target;
  this.setState({[name]: this.state[name] += 1})
- console.log(this.state)
+ 
+this.countTotalFeedback()
+       console.log(this.state)
       }
+      countTotalFeedback =() =>{
+        let sum = this.state.good + this.state.bad + this.state.neutral
+       this.setState({total : sum})
+    
+      }
+     
       render() {
         return (
             <div>
             <h2>Please leave feedback</h2>
-            <button type="button" name="good"onClick={this.hendleClick} >Good</button>
+            <button type="button" name="good"onClick={ this.hendleClick  } >Good</button>
             <button type="button" name="neutral" onClick={this.hendleClick}>Neutral</button>
             <button type="button" name="bad" onClick={this.hendleClick}>Bad</button>
             <h2>Statistics</h2>
             <span>Good:{this.state.good}</span>
             <span>Neutral:{this.state.neutral}</span>
             <span>Bad:{this.state.bad}</span>
+            <span>total:{this.state.total}</span>
+            
             </div>
           );
         
